1b55ab2a1cc2cd8ac7d0e2e2f03e317ae9f5a6ff
pdol <111592903+pdol9@users.noreply.github.com>
2026-05-23 21:46:35 +0200

    add ex03: zoomed image

---
+++ 1/ex03/zoom.py
@@ -0,0 +1,47 @@
+from load_image import load_image
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+
+def zoom(img: np.ndarray) -> None:
+    """ 'Zoom' into the image and display information. """
+
+    try:
+        if img is None:
+            raise ValueError("Empty image")
+
+        # crop the image
+        zoomed = img[100:500, 450:850]
+
+        # grayscale
+        zoomed = cv2.cvtColor(zoomed, cv2.COLOR_RGB2GRAY)
+
+        print("New shape after slicing:", zoomed.shape)
+        print(zoomed)
+
+        # display with pixel scale
+        plt.imshow(zoomed, cmap="gray")
+
+        plt.xlabel("X axis (pixels)")
+        plt.ylabel("Y axis (pixels)")
+        plt.title("Zoomed Image")
+
+        # optional tick spacing
+        plt.xticks(range(0, 401, 50))
+        plt.yticks(range(0, 401, 50))
+
+        # save
+        plt.savefig("zoom.png")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    try:
+        img_arr = load_image("animal.jpeg")
+        zoomed = zoom(img_arr)
+
+    except Exception as e:
+        print(f"Error: {e}")
